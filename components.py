3aba91997dda96bf0f16fe100b36da7a17a35366
Gleb Kushedow <kushedow@gmail.com>
2026-05-02 11:52:57 +0300

    added Readme

--- components.py
+++ components.py
@@ -1,14 +1,6 @@
 import socketio
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-
-from src.api.routes import router
-
 fast_app = FastAPI()
-
-fast_app.mount("/src/static", StaticFiles(directory="src/static"), name="static")
-
-fast_app.include_router(router)
 
 sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
 app = socketio.ASGIApp(sio, other_asgi_app=fast_app)
--- src/config.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-
-FONT_PATH = Path(__file__).parent / "static" / "fonts" / "Roboto-Bold.ttf"
-PICTURE_PATH = Path(__file__).parent / "static" / "img.png"
